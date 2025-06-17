7e62696a91316682663082b89ba31bb147cbd085
Filip Skoczen <philipskoczen@gmail.com>
2025-06-17 18:28:33 +0200

    feat: Make game routes client only

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,31 +2,37 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
 
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/test-utils', 'shadcn-nuxt', '@pinia/nuxt'],
-
-  icon: {
-    componentName: 'NuxtIcon',
-  },
-  eslint: {
-    config: {
-      stylistic: true,
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  routeRules: {
+    '/game/**': {
+      ssr: false,
     },
+    '/': { prerender: true },
   },
   future: {
     compatibilityVersion: 4,
   },
-  css: ['~/assets/css/main.css'],
+  compatibilityDate: '2025-05-15',
   vite: {
     plugins: [
       // tailwindcss plugin cannot satisfy PluginOption type event if it is fully compatibile
       (tailwindcss as Function)(),
     ],
   },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+
+  icon: {
+    componentName: 'NuxtIcon',
+  },
   shadcn: {
     prefix: 'Ui',
-    componentDir: './components/ui'
-  }
-})
+    componentDir: './components/ui',
+  },
+});
